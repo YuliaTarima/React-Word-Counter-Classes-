cb754b36a43e7dffb7b273968dc1bffafa48fb4b
Yulia Tarima <yulia.tarima@gmail.com>
2020-10-16 10:11:01 -0700

    basic word counter functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProgressBar from './ProgressBar';
+import Counter from './Counter';
+import Editor from './Editor';
+import SaveManager from './SaveManager';
+import countWords from './countWords';
+import makeFakeRequest from './makeFakeRequest';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {text: ''};
+        this.handleTextChange = this.handleTextChange.bind(this);
+    }
+
+    handleTextChange(currentText) {
+        this.setState(() => ({text: currentText}));
+    }
+
+    render() {
+        const {targetWordCount} = this.props;
+        const {text} = this.state;
+        const wordCount = countWords(text);
+        const progress = wordCount / targetWordCount;
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        React word counter with progress bar defaulting to 20 words
+                    </p>
+                </header>
+
+                <form className="measure pa4 sans-serif">
+                    <Editor
+                        onTextChange={this.handleTextChange}
+                        text={text}
+                    />
+                    <Counter count={wordCount}/>
+                    <ProgressBar completion={progress}/>
+                    <SaveManager saveFunction={makeFakeRequest}
+                                 data={this.state}/>
+                </form>
+            </div>
+        );
+    }
+
 }
 
 export default App;
